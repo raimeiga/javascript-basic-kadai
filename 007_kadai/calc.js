3725af79039732a8e93923e9addfb9c7f258ef94
raimeiga <yusuke612@hotmail.com>
2023-07-02 13:59:54 +0900

    switchからif文に修正

---
+++ 007_kadai/calc.js
@@ -0,0 +1,17 @@
+let num = Math.floor(Math.random() * 16);
+
+if (num === 3) {
+    console.log('3の倍数です');
+  }
+
+if (num === 5) {
+    console.log('5の倍数です');
+  }
+  
+else if (num === 15) {
+    console.log('3と5の倍数です');
+  }
+  
+else {
+    console.log(num);
+  }
