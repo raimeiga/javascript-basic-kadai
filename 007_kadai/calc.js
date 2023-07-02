168516e71213cc3f95855133ed25a52595c78c54
raimeiga <yusuke612@hotmail.com>
2023-07-02 14:15:44 +0900

    if文増やした

--- 007_kadai/calc.js
+++ 007_kadai/calc.js
@@ -1,10 +1,10 @@
 let num = Math.floor(Math.random() * 16);
 
-if (num === 3) {
+if (num === 3 6 9) {
     console.log('3の倍数です');
   }
 
-if (num === 5) {
+if (num === 5 10 15) {
     console.log('5の倍数です');
   }
   
