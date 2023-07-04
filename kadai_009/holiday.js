dd6771654106fffcff8d08129e808cf0ec43fc1f
raimeiga <yusuke612@hotmail.com>
2023-07-04 11:22:22 +0900

    for文条件式を <=  から < に変更

--- kadai_009/holiday.js
+++ kadai_009/holiday.js
@@ -1,6 +1,6 @@
 let holidays= ["正月","成人の日","建国記念日","天皇誕生日","春分の日","昭和の日","憲法記念日","みどりの日","こどもの日","海の日","山の日","敬老の日","秋分の日","スポーツの日","文化の日","勤労感謝の日"];
 
-for(let i = 0; i <= holidays.length; i++) {
+for(let i = 0; i < holidays.length; i++) {
   console.log(holidays[i]);
 }
 
@@ -9,4 +9,6 @@ while (index < holidays.length) {
   console.log(holidays[index]);
   index++;
   }
+
+//「for文をwhile文へ」という検索で方法を調べてみた  
 
