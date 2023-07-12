276f9abd9f796598184d5b1eefb04ffb78306f3b
raimeiga <yusuke612@hotmail.com>
2023-07-12 15:10:00 +0900

    模範解答を追加

--- kadai_019/event.js
+++ kadai_019/event.js
@@ -5,3 +5,12 @@ btn.addEventListener('mousedown', ()=> {
 
     h2.textContent = 'ボタンをクリックしました';
 });
+
+/* 模範解答コード
+const btn = document.getElementById('btn');
+const text = document.getElementById('text');
+
+btn.addEventListener('click', () => {
+    text.textContent = "ボタンをクリックしました";
+});
+*/
