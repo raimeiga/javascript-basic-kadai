d278568de26c278a82577e4be86fb103d2667026
raimeiga <yusuke612@hotmail.com>
2023-07-13 22:39:26 +0900

    課題の訂正

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,13 +1,9 @@
 const h2 = document.getElementById('text');
 const btn = document.getElementById('btn');
 
-/*
-btn.addEventListener('click', setTimeout(() => {
+btn.addEventListener('click',() => {
+    setTimeout(() => {
     h2.textContent = 'ボタンをクリックしました';
-}, 2000)
+    }, 2000)
+  }
 );
-*/
-
-setTimeout(btn.addEventListener('click',() => {
-    h2.textContent = 'ボタンをクリックしました';
-}),2000);
