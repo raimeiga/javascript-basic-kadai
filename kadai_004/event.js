5f5ee33f7eb241e2bd869fbb634f340de16f54c1
raimeiga <yusuke612@hotmail.com>
2023-08-28 12:56:50 +0900

    kadai_004コード作成

---
+++ kadai_004/event.js
@@ -0,0 +1,12 @@
+$(function (){
+    $(window).on('load',function(){
+        console.log('loadイベントが発生しました');
+    })
+
+    $(window).on('scroll',function(){
+        const scroollValue = document.scrollingElement.scrollTop
+        if(scroollValue > 10){
+            console.log('scrollイベントが発生しました');
+        }
+    });
+    });
