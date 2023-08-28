edec1f2355bab23d1a39f45293c42b5e94edb95a
raimeiga <yusuke612@hotmail.com>
2023-08-28 13:40:21 +0900

    funcの中にだけloadがある状態

--- kadai_004/event.js
+++ kadai_004/event.js
@@ -1,13 +1,12 @@
 $(function (){
     $(window).on('load',function(){
         console.log('loadイベントが発生しました');
-    })
+    });
+});
 
-    $(window).on('scroll',function(){
+$(window).on('scroll',function(){
         const scroollValue = document.scrollingElement.scrollTop
         if(scroollValue > 10){
             console.log('scrollイベントが発生しました');
         }
     });
-    });
-    
